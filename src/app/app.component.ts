909ebd1042ce670e307d230ceae708005e56ba23
Emir Basic <ebasic@gmail.com>
2020-07-18 09:54:34 +0200

    #179. Improving the Reactive Service with Observables (Subjects)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
-
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string): void {
-    this.loadedFeature = feature;
-  }
 }
